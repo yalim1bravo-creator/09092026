8a91905e06da9da29928bee95337d3df34779b73
yalim1bravo-creator <yalim1bravo@gmail.com>
2026-09-09 13:17:42 -0600

    Mensaje descriptivo

--- index.php
+++ index.php
@@ -4,8 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="src/estilo.css">
+    <!-- fuentes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Autour+One&display=swap" rel="stylesheet">
+
+<!-- bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+     <!-- bootstrap icon -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
 </head>
 <body>
-    
+    <!-- header -->
+     <header>
+        <div class="container">
+            <h2 class="titulo"> Este es el header</h2>
+            <p>Ejemplo de estructura CSS</p>
+        </div>
+        <!-- navbar -->
+        <nav>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Articulo</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Pibbles</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class=" cuerpo">
+        <div class="row">
+            <div class="col">
+                <h2 class="titulo">Este es el titulo</h2>
+                <p class="texto">Este es el texto</p>
+                <p class="texto">Este es el texto</p>
+                <p class="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum animi illo pariatur perferendis inventore praesentium neque modi unde eligendi, obcaecati asperiores suscipit repudiandae eveniet in. Vitae ullam fugit neque qui.
+                Quisquam totam iusto nemo quaerat est exercitationem pariatur. Quam doloremque labore necessitatibus possimus enim temporibus, itaque explicabo unde suscipit earum ipsum perspiciatis debitis magnam a accusantium excepturi error veritatis et.
+                Sed autem doloremque vitae magnam optio beatae totam commodi nesciunt? Quidem recusandae quo porro aspernatur earum possimus repellendus, sed, libero dolores similique accusamus eum, laborum non ipsam cum maxime debitis.</p>
+
+            </div>
+        </div>
+    </div>
 </body>
+<footer>
+        <div class="footer">
+            <div class="row">
+                <div class="col footer-col"><i class="bi bi-facebook icon"></i><p class="texto-footer">Facebook</p></div>
+                <div class="col footer-col"><i class="bi bi-youtube icon"></i><p class="texto-footer">Youtube</p></div>
+                <div class="col footer-col"><i class="bi bi-whatsapp icon"></i><p class="texto-footer">Whatsapp</p></div>
+            </div>
+        </div>
+</footer>
 </html>
